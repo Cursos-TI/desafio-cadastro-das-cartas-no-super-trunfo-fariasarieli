5d9f7dad2cd9dba725233f53dd6fc6ec42e71aae
fariasarieli <farias.arieli@gmail.com>
2025-09-10 01:45:05 +0000

    inicio

--- Cartasupertrunfo.c
+++ Cartasupertrunfo.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 
 int main (){
+    char estadocarta1[50];
+    char codigocarta1[4];
+    int populaçãocarta1;
+    float areacarta1;
+    float pibcarta1;
+    int pontoscarta1;
+
+    char estadocarta2[50];
+    char codigocarta2[4];
+    int populaçãocarta2;
+    int populaçãocarta2;
+    float areacarta2;
+    float pibcarta2;
+    int pontoscarta2;
 
     printf ("Super Trunfo - Países!\n");
-    
+    printf ("Estado: \n");
+    scanf ("%s", &estadocarta1);
+
+
+
+
 }
 
 
